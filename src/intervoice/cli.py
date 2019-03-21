2d06aab002c731b26debaaa8e350846cbcf7c135
akb <EYoS2GqgJf27U0nEnfCM-noreply@cordaz.com>
2019-03-21 04:10:02 +0000

    Use click for cli.mic

--- src/intervoice/cli.py
+++ src/intervoice/cli.py
@@ -14,10 +14,29 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
+import types
+
 import click
 
 from intervoice import app
+from intervoice import mic
 
-@click.command()
+
+@click.group()
 def cli(**kwargs):
     app.main(**kwargs)
+
+
+@cli.command("mic")
+@click.option("--server", "-s", default="127.0.0.1")
+@click.option("--port", "-p", default=8019, type=int)
+@click.option("--device", "-d", default=7, type=int)
+@click.option("--hypotheses", type=bool, default=True)
+@click.option("--save-adaptation-state", type=bool)
+@click.option("--send-adaptation-state", type=bool)
+@click.option("--audio-gate", default=0, type=int)
+def _(**kwargs):
+    content_type = "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1"
+    path = "client/ws/speech"
+
+    mic.run(args=types.SimpleNamespace(**kwargs), path=path, content_type=content_type)
