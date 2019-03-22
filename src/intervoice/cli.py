43080fee6dd04b0cd55698108d2c41e60cd02cee
akb <EYoS2GqgJf27U0nEnfCM-noreply@cordaz.com>
2019-03-22 23:23:50 +0000

    Add `mic` command for detecting microphone

--- src/intervoice/cli.py
+++ src/intervoice/cli.py
@@ -14,24 +14,31 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
+import sys
 import types
 
 import click
 
 
 from intervoice import app
-from intervoice import mic
+from intervoice import listen
 from intervoice import manager
 from intervoice import worker
 
 
 @click.group()
-@click.option('--log/--no-log', is_flag=True, default=True)
+@click.option("--log/--no-log", is_flag=True, default=True)
 def cli(**kwargs):
     app.main(**kwargs)
 
 
 @cli.command("mic")
+def _detect_mic(**kwargs):
+    del sys.argv[1]
+    import intervoice.mic
+
+
+@cli.command("listen")
 @click.option("--server", "-s", default="127.0.0.1")
 @click.option("--port", "-p", default=8019, type=int)
 @click.option("--device", "-d", default=7, type=int)
@@ -39,11 +46,13 @@ def cli(**kwargs):
 @click.option("--save-adaptation-state", type=bool)
 @click.option("--send-adaptation-state", type=bool)
 @click.option("--audio-gate", default=0, type=int)
-def _mic(**kwargs):
+def _listen(**kwargs):
     content_type = "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1"
     path = "client/ws/speech"
 
-    mic.run(args=types.SimpleNamespace(**kwargs), path=path, content_type=content_type)
+    listen.run(
+        args=types.SimpleNamespace(**kwargs), path=path, content_type=content_type
+    )
 
 
 @cli.command("manage")
--- src/intervoice/mic.py
+++ src/intervoice/mic.py
@@ -1,265 +1,152 @@
-# Silvius microphone client based on Tanel's client.py
-__author__ = "dwk"
+#!/usr/bin/env python3
+"""Plot the live microphone signal(s) with matplotlib.
+
+Matplotlib and NumPy have to be installed.
+
+
+This module is from the sounddevice docs.
+"""
+
 
 import argparse
-from ws4py.client.threadedclient import WebSocketClient
-import threading
+import queue
 import sys
-import urllib.request, urllib.parse, urllib.error
-import json
-
-reconnect_mode = False
-fatal_error = False
 
 
-class MyClient(WebSocketClient):
-    def __init__(
-        self,
-        url,
-        mic=1,
-        protocols=None,
-        extensions=None,
-        heartbeat_freq=None,
-        byterate=16000,
-        show_hypotheses=True,
-        save_adaptation_state_filename=None,
-        send_adaptation_state_filename=None,
-        audio_gate=0,
-    ):
-        super(MyClient, self).__init__(url, protocols, extensions, heartbeat_freq)
-        self.mic = mic
-        self.show_hypotheses = show_hypotheses
-        self.byterate = byterate
-        self.save_adaptation_state_filename = save_adaptation_state_filename
-        self.send_adaptation_state_filename = send_adaptation_state_filename
-        self.chunk = 0
-        self.audio_gate = audio_gate
-
-    def send_data(self, data):
-        self.send(data, binary=True)
-
-    def opened(self):
-        import pyaudio
-        import audioop
-
-        pa = pyaudio.PyAudio()
-        sample_rate = self.byterate
-        stream = None
-
-        while stream is None:
-            try:
-                # try adjusting this if you want fewer network packets
-                self.chunk = 2048 * 2 * sample_rate // self.byterate
-
-                mic = self.mic
-                if mic == -1:
-                    mic = pa.get_default_input_device_info()["index"]
-                    print("Selecting default mic", file=sys.stderr)
-                print("Using mic #", mic, file=sys.stderr)
-                stream = pa.open(
-                    rate=sample_rate,
-                    format=pyaudio.paInt16,
-                    channels=1,
-                    input=True,
-                    input_device_index=mic,
-                    frames_per_buffer=self.chunk,
-                )
-            except IOError as e:
-                if e.errno == -9997 or e.errno == "Invalid sample rate":
-                    new_sample_rate = int(
-                        pa.get_device_info_by_index(mic)["defaultSampleRate"]
-                    )
-                    if sample_rate != new_sample_rate:
-                        sample_rate = new_sample_rate
-                        continue
-                print("\n", e, file=sys.stderr)
-                print(
-                    "\nCould not open microphone. Please try a different device.",
-                    file=sys.stderr,
-                )
-                global fatal_error
-                fatal_error = True
-                sys.exit(0)
-
-        def mic_to_ws():  # uses stream
-            try:
-                print("\nLISTENING TO MICROPHONE", file=sys.stderr)
-                last_state = None
-                while True:
-                    data = stream.read(self.chunk)
-                    if self.audio_gate > 0:
-                        rms = audioop.rms(data, 2)
-                        if rms < self.audio_gate:
-                            data = "\00" * len(data)
-                    # if sample_chan == 2:
-                    #    data = audioop.tomono(data, 2, 1, 1)
-                    if sample_rate != self.byterate:
-                        (data, last_state) = audioop.ratecv(
-                            data, 2, 1, sample_rate, self.byterate, last_state
-                        )
-
-                    self.send_data(data)
-            except IOError as e:
-                # usually a broken pipe
-                print(e)
-            except AttributeError:
-                # currently raised when the socket gets closed by main thread
-                pass
-
-            # to voluntarily close the connection, we would use
-            # self.send_data("")
-            # self.send("EOS")
-
-            try:
-                self.close()
-            except IOError:
-                pass
-
-        threading.Thread(target=mic_to_ws).start()
-
-    def received_message(self, m):
-        response = json.loads(str(m))
-        # print >> sys.stderr, "RESPONSE:", response
-        # print >> sys.stderr, "JSON was:", m
-        if response["status"] == 0:
-            if "result" in response:
-                trans = response["result"]["hypotheses"][0]["transcript"]
-                if response["result"]["final"]:
-                    if self.show_hypotheses:
-                        print("\r%s" % trans.replace("\n", "\\n"), file=sys.stderr)
-                    print("%s" % trans.replace("\n", "\\n"))  # final result!
-                    sys.stdout.flush()
-                elif self.show_hypotheses:
-                    print_trans = trans.replace("\n", "\\n")
-                    if len(print_trans) > 80:
-                        print_trans = "... %s" % print_trans[-76:]
-                    print("\r%s" % print_trans, end=" ", file=sys.stderr)
-            if "adaptation_state" in response:
-                if self.save_adaptation_state_filename:
-                    print(
-                        "Saving adaptation state to %s"
-                        % self.save_adaptation_state_filename,
-                        file=sys.stderr,
-                    )
-                    with open(self.save_adaptation_state_filename, "w") as f:
-                        f.write(json.dumps(response["adaptation_state"]))
-        else:
-            print(
-                "Received error from server (status %d)" % response["status"],
-                file=sys.stderr,
-            )
-            if "message" in response:
-                print("Error message:", response["message"], file=sys.stderr)
-
-            global reconnect_mode
-            if reconnect_mode:
-                import time
-
-                print("Sleeping for five seconds before reconnecting", file=sys.stderr)
-                time.sleep(5)
-
-    def closed(self, code, reason=None):
-        # print "Websocket closed() called"
-        # print >> sys.stderr
-        pass
-
-
-def setup():
-    content_type = "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1"
-    path = "client/ws/speech"
-
-    parser = argparse.ArgumentParser(description="Microphone client for silvius")
-    parser.add_argument(
-        "-s",
-        "--server",
-        default="localhost",
-        dest="server",
-        help="Speech-recognition server",
-    )
-    parser.add_argument("-p", "--port", default="8019", dest="port", help="Server port")
-    # parser.add_argument('-r', '--rate', default=16000, dest="rate", type=int, help="Rate in bytes/sec at which audio should be sent to the server.")
-    parser.add_argument(
-        "-d",
-        "--device",
-        default="-1",
-        dest="device",
-        type=int,
-        help="Select a different microphone (give device ID)",
-    )
-    parser.add_argument(
-        "-k",
-        "--keep-going",
-        action="store_true",
-        help="Keep reconnecting to the server after periods of silence",
-    )
-    parser.add_argument("--save-adaptation-state", help="Save adaptation state to file")
-    parser.add_argument(
-        "--send-adaptation-state", help="Send adaptation state from file"
-    )
-    parser.add_argument(
-        "--content-type",
-        default=content_type,
-        help="Use the specified content type (default is " + content_type + ")",
-    )
-    parser.add_argument(
-        "--hypotheses",
-        default=True,
-        type=int,
-        help="Show partial recognition hypotheses (default: 1)",
-    )
-    parser.add_argument(
-        "-g",
-        "--audio-gate",
-        default=0,
-        type=int,
-        help="Audio-gate level to reduce detections when not talking",
-    )
-    args = parser.parse_args()
-
-    content_type = args.content_type
-    print("Content-Type:", content_type, file=sys.stderr)
-
-    if args.keep_going:
-        global reconnect_mode
-        global fatal_error
-        reconnect_mode = True
-        while fatal_error == False:
-            print("Reconnecting...", file=sys.stderr)
-            run(args, content_type, path)
-    else:
-        run(args, content_type, path)
-
-
-def run(args, content_type, path):
-    uri = "ws://%s:%s/%s?%s" % (
-        args.server,
-        args.port,
-        path,
-        urllib.parse.urlencode([("content-type", content_type)]),
-    )
-    print("Connecting to", uri, file=sys.stderr)
-
-    ws = MyClient(
-        uri,
-        byterate=16000,
-        mic=args.device,
-        show_hypotheses=args.hypotheses,
-        save_adaptation_state_filename=args.save_adaptation_state,
-        send_adaptation_state_filename=args.send_adaptation_state,
-        audio_gate=args.audio_gate,
-    )
-    ws.connect()
-    # result = ws.get_full_hyp()
-    # print result.encode('utf-8')
-    ws.run_forever()
-
-
-def main():
+def int_or_str(text):
+    """Helper function for argument parsing."""
     try:
-        setup()
-    except KeyboardInterrupt:
-        print("\nexiting...", file=sys.stderr)
+        return int(text)
+    except ValueError:
+        return text
 
 
-if __name__ == "__main__":
-    main()
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument(
+    "-l",
+    "--list-devices",
+    action="store_true",
+    help="show list of audio devices and exit",
+)
+parser.add_argument(
+    "-d", "--device", type=int_or_str, help="input device (numeric ID or substring)"
+)
+parser.add_argument(
+    "-w",
+    "--window",
+    type=float,
+    default=200,
+    metavar="DURATION",
+    help="visible time slot (default: %(default)s ms)",
+)
+parser.add_argument(
+    "-i",
+    "--interval",
+    type=float,
+    default=30,
+    help="minimum time between plot updates (default: %(default)s ms)",
+)
+parser.add_argument("-b", "--blocksize", type=int, help="block size (in samples)")
+parser.add_argument(
+    "-r", "--samplerate", type=float, help="sampling rate of audio device"
+)
+parser.add_argument(
+    "-n",
+    "--downsample",
+    type=int,
+    default=10,
+    metavar="N",
+    help="display every Nth sample (default: %(default)s)",
+)
+parser.add_argument(
+    "channels",
+    type=int,
+    default=[1],
+    nargs="*",
+    metavar="CHANNEL",
+    help="input channels to plot (default: the first)",
+)
+args = parser.parse_args()
+if any(c < 1 for c in args.channels):
+    parser.error("argument CHANNEL: must be >= 1")
+mapping = [c - 1 for c in args.channels]  # Channel numbers start with 1
+q = queue.Queue()
+
+
+def audio_callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    # Fancy indexing with mapping creates a (necessary!) copy:
+    q.put(indata[:: args.downsample, mapping])
+
+
+def update_plot(frame):
+    """This is called by matplotlib for each plot update.
+
+    Typically, audio callbacks happen more frequently than plot updates,
+    therefore the queue tends to contain multiple blocks of audio data.
+
+    """
+    global plotdata
+    while True:
+        try:
+            data = q.get_nowait()
+        except queue.Empty:
+            break
+        shift = len(data)
+        plotdata = np.roll(plotdata, -shift, axis=0)
+        plotdata[-shift:, :] = data
+    for column, line in enumerate(lines):
+        line.set_ydata(plotdata[:, column])
+    return lines
+
+
+try:
+    from matplotlib.animation import FuncAnimation
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import sounddevice as sd
+
+    if args.list_devices:
+        print(sd.query_devices())
+        parser.exit(0)
+    if args.samplerate is None:
+        device_info = sd.query_devices(args.device, "input")
+        args.samplerate = device_info["default_samplerate"]
+
+    length = int(args.window * args.samplerate / (1000 * args.downsample))
+    plotdata = np.zeros((length, len(args.channels)))
+
+    fig, ax = plt.subplots()
+    lines = ax.plot(plotdata)
+    if len(args.channels) > 1:
+        ax.legend(
+            ["channel {}".format(c) for c in args.channels],
+            loc="lower left",
+            ncol=len(args.channels),
+        )
+    ax.axis((0, len(plotdata), -1, 1))
+    ax.set_yticks([0])
+    ax.yaxis.grid(True)
+    ax.tick_params(
+        bottom=False,
+        top=False,
+        labelbottom=False,
+        right=False,
+        left=False,
+        labelleft=False,
+    )
+    fig.tight_layout(pad=0)
+
+    stream = sd.InputStream(
+        device=args.device,
+        channels=max(args.channels),
+        samplerate=args.samplerate,
+        callback=audio_callback,
+    )
+    ani = FuncAnimation(fig, update_plot, interval=args.interval, blit=True)
+    with stream:
+        plt.show()
+except Exception as e:
+    parser.exit(type(e).__name__ + ": " + str(e))
