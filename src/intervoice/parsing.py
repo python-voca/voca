e6f1346a9578adc2edc0f012e5ad8bc10f4f1b5a
akb <EYoS2GqgJf27U0nEnfCM-noreply@cordaz.com>
2019-04-19 04:55:15 +0000

    Add extra whitespace in grammar

--- src/intervoice/parsing.py
+++ src/intervoice/parsing.py
@@ -50,7 +50,7 @@ def build_grammar(registry: utils.Registry, rules: List[utils.Rule]) -> str:
 
     message = "?message : " + "|".join(rule.name for rule in rules)
 
-    rules_segment = "\n".join([f"{rule.name}:{rule.pattern}" for rule in rules])
+    rules_segment = "\n".join([f"{rule.name} : {rule.pattern}" for rule in rules])
     patterns_segment = "\n".join([f"{k}:{v}" for k, v in registry.patterns.items()])
     body = "\n".join([message, rules_segment, patterns_segment])
 
