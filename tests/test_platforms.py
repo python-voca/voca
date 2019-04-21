354866eb71bb8e272e30646800e7bc09d536641f
akb <EYoS2GqgJf27U0nEnfCM-noreply@cordaz.com>
2019-04-21 23:21:12 +0000

    Test implementation dict is accessible

--- tests/test_platforms.py
+++ tests/test_platforms.py
@@ -3,17 +3,24 @@ import platform
 from intervoice import platforms
 
 
+@platforms.implementation(platforms.Platform.LINUX)
+def get_platform():
+    return "This is Linux"
+
+
+@platforms.implementation(platforms.Platform.DARWIN)
+def get_platform():
+    return "This is Darwin"
+
+
+@platforms.implementation(platforms.Platform.WINDOWS)
+def get_platform():
+    return "This is Windows"
+
+
 def test_implementation():
-    @platforms.implementation(platforms.Platform.LINUX)
-    def get_platform():
-        return "This is Linux"
-
-    @platforms.implementation(platforms.Platform.DARWIN)
-    def get_platform():
-        return "This is Darwin"
-
-    @platforms.implementation(platforms.Platform.WINDOWS)
-    def get_platform():
-        return "This is Windows"
-
     assert get_platform() == "This is " + platform.system()
+
+
+def test_dict_accessible():
+    assert len(set(get_platform.implementations)) == 3
