f7d188b3f01462ca0d7d066011762c7f0daafb32
akb <EYoS2GqgJf27U0nEnfCM-noreply@cordaz.com>
2019-04-21 23:16:59 +0000

    Fix platform test

--- tests/test_platforms.py
+++ tests/test_platforms.py
@@ -2,19 +2,18 @@ import platform
 
 from intervoice import platforms
 
+
 def test_implementation():
     @platforms.implementation(platforms.Platform.LINUX)
     def get_platform():
-        return 'This is Linux'
+        return "This is Linux"
 
-    a = get_platform
     @platforms.implementation(platforms.Platform.DARWIN)
     def get_platform():
-        return 'This is Darwin'
+        return "This is Darwin"
 
     @platforms.implementation(platforms.Platform.WINDOWS)
     def get_platform():
-        return 'This is Windows'
+        return "This is Windows"
 
-    assert get_platform is a
-    assert get_platform() == 'This is ' + platform.system()
+    assert get_platform() == "This is " + platform.system()
