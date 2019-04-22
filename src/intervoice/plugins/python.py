06b6dcd5a27cc7020f5c4c5c3f0aa070b0d5b4f7
akb <EYoS2GqgJf27U0nEnfCM-noreply@cordaz.com>
2019-04-22 00:45:47 +0000

    Rename caster_compat to casterlike

--- src/intervoice/plugins/python.py
+++ src/intervoice/plugins/python.py
@@ -1,7 +1,7 @@
-from intervoice.caster_compat import R
-from intervoice.caster_compat import Text
-from intervoice.caster_compat import Key
-from intervoice.caster_compat import add_to_registry
+from intervoice.casterlike import R
+from intervoice.casterlike import Text
+from intervoice.casterlike import Key
+from intervoice.casterlike import add_to_registry
 
 
 from intervoice import utils
