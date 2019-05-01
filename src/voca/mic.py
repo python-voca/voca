f113a42be3d8ee4a44a5dc89914ca97eae9472b6
akb <EYoS2GqgJf27U0nEnfCM-noreply@cordaz.com>
2019-05-01 19:05:47 +0000

    Parse mic args in __name__ block

--- src/voca/mic.py
+++ src/voca/mic.py
@@ -21,6 +21,7 @@ def int_or_str(text):
         return text
 
 
+
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
     "-l",
@@ -66,11 +67,7 @@ parser.add_argument(
     metavar="CHANNEL",
     help="input channels to plot (default: the first)",
 )
-args = parser.parse_args()
-if any(c < 1 for c in args.channels):
-    parser.error("argument CHANNEL: must be >= 1")
-mapping = [c - 1 for c in args.channels]  # Channel numbers start with 1
-q = queue.Queue()
+
 
 
 def audio_callback(indata, frames, time, status):
@@ -101,52 +98,60 @@ def update_plot(frame):
         line.set_ydata(plotdata[:, column])
     return lines
 
+if __name__ == '__main__':
+    args = parser.parse_args()
 
-try:
-    from matplotlib.animation import FuncAnimation
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import sounddevice as sd
+    if any(c < 1 for c in args.channels):
+        parser.error("argument CHANNEL: must be >= 1")
+    mapping = [c - 1 for c in args.channels]  # Channel numbers start with 1
+    q = queue.Queue()
 
-    if args.list_devices:
-        print(sd.query_devices())
-        parser.exit(0)
-    if args.samplerate is None:
-        device_info = sd.query_devices(args.device, "input")
-        args.samplerate = device_info["default_samplerate"]
 
-    length = int(args.window * args.samplerate / (1000 * args.downsample))
-    plotdata = np.zeros((length, len(args.channels)))
+    try:
+        from matplotlib.animation import FuncAnimation
+        import matplotlib.pyplot as plt
+        import numpy as np
+        import sounddevice as sd
 
-    fig, ax = plt.subplots()
-    lines = ax.plot(plotdata)
-    if len(args.channels) > 1:
-        ax.legend(
-            ["channel {}".format(c) for c in args.channels],
-            loc="lower left",
-            ncol=len(args.channels),
+        if args.list_devices:
+            print(sd.query_devices())
+            parser.exit(0)
+        if args.samplerate is None:
+            device_info = sd.query_devices(args.device, "input")
+            args.samplerate = device_info["default_samplerate"]
+
+        length = int(args.window * args.samplerate / (1000 * args.downsample))
+        plotdata = np.zeros((length, len(args.channels)))
+
+        fig, ax = plt.subplots()
+        lines = ax.plot(plotdata)
+        if len(args.channels) > 1:
+            ax.legend(
+                ["channel {}".format(c) for c in args.channels],
+                loc="lower left",
+                ncol=len(args.channels),
+            )
+        ax.axis((0, len(plotdata), -1, 1))
+        ax.set_yticks([0])
+        ax.yaxis.grid(True)
+        ax.tick_params(
+            bottom=False,
+            top=False,
+            labelbottom=False,
+            right=False,
+            left=False,
+            labelleft=False,
         )
-    ax.axis((0, len(plotdata), -1, 1))
-    ax.set_yticks([0])
-    ax.yaxis.grid(True)
-    ax.tick_params(
-        bottom=False,
-        top=False,
-        labelbottom=False,
-        right=False,
-        left=False,
-        labelleft=False,
-    )
-    fig.tight_layout(pad=0)
+        fig.tight_layout(pad=0)
 
-    stream = sd.InputStream(
-        device=args.device,
-        channels=max(args.channels),
-        samplerate=args.samplerate,
-        callback=audio_callback,
-    )
-    ani = FuncAnimation(fig, update_plot, interval=args.interval, blit=True)
-    with stream:
-        plt.show()
-except Exception as e:
-    parser.exit(type(e).__name__ + ": " + str(e))
+        stream = sd.InputStream(
+            device=args.device,
+            channels=max(args.channels),
+            samplerate=args.samplerate,
+            callback=audio_callback,
+        )
+        ani = FuncAnimation(fig, update_plot, interval=args.interval, blit=True)
+        with stream:
+            plt.show()
+    except Exception as e:
+        parser.exit(type(e).__name__ + ": " + str(e))
